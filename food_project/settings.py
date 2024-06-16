08a8f2c5948f0ee6f25a59297b3542eb1b9b42ea
Pawan Gusain <pawangusain1078@gmail.com>
2024-06-17 04:28:22 +0530

    settings.py

--- food_project/settings.py
+++ food_project/settings.py
@@ -122,12 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-if not DEBUG:
-    STATICFILES_DIRS=[
-        os.path.join(BASE_DIR, 'static')
-    ]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'  # Assuming you have this defined
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL='/dishimage/'
