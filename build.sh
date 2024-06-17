7593c6f8f1d4a8f206538307b2c18ef1e01bb0a9
Pawan Gusain <pawangusain1078@gmail.com>
2024-06-17 23:44:23 +0530

    build issues fixed

--- build.sh
+++ build.sh
@@ -5,4 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 echo "Build completed successfully."
-python manage.py createsuperuser 
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@email.com
+export DJANGO_SUPERUSER_PASSWORD="adpass"  # Replace with your actual password
+
+# Create superuser if it doesn't exist
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
+
+# Clear sensitive environment variables
+unset DJANGO_SUPERUSER_PASSWORD
