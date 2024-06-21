e25f3882cfed840315d3b1a67c8f605f0fed1bf4
Pawan Gusain <139328839+pawan0905@users.noreply.github.com>
2024-06-21 22:44:03 +0530

    Update settings.py

--- food_project/settings.py
+++ food_project/settings.py
@@ -22,13 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#4-7um!*w9cyznnq6#kmsc6h+(!8$ozgw&n)eas#k$=ass)!$e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG" ,"False").lower() == "true"
-
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
