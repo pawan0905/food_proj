d38851aa97e200657ed3fd60903ce972848fa3da
Pawan Gusain <139328839+pawan0905@users.noreply.github.com>
2024-07-14 22:44:31 +0530

    Update urls.py

--- food_project/urls.py
+++ food_project/urls.py
@@ -17,13 +17,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+# from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('food_app.urls')),
-    re_path(r'^dishimage/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^dishimage/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
