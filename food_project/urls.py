959cecfbd5509ac6c5607bd04a0cd23b7ed45901
Pawan Gusain <139328839+pawan0905@users.noreply.github.com>
2024-07-14 22:31:33 +0530

    Update urls.py

--- food_project/urls.py
+++ food_project/urls.py
@@ -14,14 +14,18 @@
 #     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('food_app.urls')),
-    re_path(r'^dishimage/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-] 
+    path('', include('food_app.urls')),
+    re_path(r'^dishimage/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
